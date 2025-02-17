f005b5dfc720991c8344612b39a7d93480ecd5e5
dhkim <dhkim@mleverage.co.kr>
2025-02-17 17:35:12 +0900

    feat: BasicButton 생성

---
+++ BasicButton/BasicButton.tsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+
+type BasicButtonProps = {
+    children: React.ReactNode; // button text
+    isVisible?: boolean;
+};
+
+const BasicButton = ({ children, isVisible }: BasicButtonProps) => {
+    const basicButtonStyle = css`
+        padding: 5px 16px;
+        box-sizing: border-box;
+        height: 40px;
+        width: auto;
+        border-radius: 8px;
+        background-color: #f98131;
+        border: none;
+        color: #fff;
+        font-family: Pretendard;
+        font-weight: 700;
+        white-space: nowrap;
+        opacity: ${isVisible ?? true ? 1 : 0};
+        transition: all 0.3s ease;
+    `;
+
+    return <button css={basicButtonStyle}>{children}</button>;
+};
+
+export default BasicButton;
