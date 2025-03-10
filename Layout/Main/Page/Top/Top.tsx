1db20194faa5a639211a637964e3bca2723c2a13
dhkim <dhkim@mleverage.co.kr>
2025-03-10 14:20:08 +0900

    feat: Top 수정 - children이 없을 경우 렌더링 하지 않도록 수정

--- Layout/Main/Page/Top/Top.tsx
+++ Layout/Main/Page/Top/Top.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Top.module.scss';
+import classNames from 'classnames';
 
 type TopProps = {
     children?: React.ReactNode;
 };
 
 const Top = ({ children }: TopProps) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [hasChildNodes, setHasChildNodes] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (containerRef.current) {
+            const observer = new MutationObserver(() => {
+                if (containerRef.current) {
+                    setHasChildNodes(containerRef.current.childNodes.length > 0);
+                }
+            });
+            observer.observe(containerRef.current, { childList: true });
+            // 초기 체크
+            setHasChildNodes(containerRef.current.childNodes.length > 0);
+            return () => observer.disconnect();
+        }
+    }, []);
+
+    const combinedClassName = classNames(styles.Top, {
+        [styles.Hidden]: !hasChildNodes,
+    });
+
     return (
-        <div id="page-top" className={styles.Top}>
+        <div id="page-top" className={combinedClassName} ref={containerRef}>
             {children}
         </div>
     );
