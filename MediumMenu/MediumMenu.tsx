1498bb47f772450ebb50c09092a22e4cbbf5bc13
dhkim <dhkim@mleverage.co.kr>
2025-03-10 10:08:24 +0900

    feat: MediumMenu 생성 - Header.Layout 제거

--- MediumMenu/MediumMenu.tsx
+++ MediumMenu/MediumMenu.tsx
@@ -1,21 +1,16 @@
-import TabGroup from "headless/TabGroup/TabGroup";
-import styles from "./MediumMenu.module.scss";
-import MediumMenuItem from "./MediumMenuItem/MediumMenuItem";
-import Layout from "headful/Layout/Layout";
+import TabGroup from 'headless/TabGroup/TabGroup';
+import styles from './MediumMenu.module.scss';
+import MediumMenuItem from './MediumMenuItem/MediumMenuItem';
 
 type MediumMenuProps = React.ComponentProps<typeof TabGroup>;
 
-const MediumMenu = (props:MediumMenuProps) => {
+const MediumMenu = (props: MediumMenuProps) => {
     return (
-        <Layout.Header>
-            <TabGroup {...props}> 
-                <div className={styles.MediumMenu}>
-                    {props.children}
-                </div>
-            </TabGroup>
-        </Layout.Header>
-    ) 
-}
+        <TabGroup {...props}>
+            <div className={styles.MediumMenu}>{props.children}</div>
+        </TabGroup>
+    );
+};
 
 export default MediumMenu;
 
