2323f8c80d466ce2cdaa4be5f2adcde322510900
dhkim <dhkim@mleverage.co.kr>
2025-03-13 18:14:33 +0900

    feat: ProfileCard 수정 - Profile 수정에 따른 코드 수정

--- ProfileCardTabGroup/ProfileCard/ProfileCard.tsx
+++ ProfileCardTabGroup/ProfileCard/ProfileCard.tsx
@@ -5,6 +5,7 @@ import FlexBox from 'headful/Flex/Flex';
 import Text from 'headful/Text/Text';
 import classNames from 'classnames';
 import { useTabGroup } from 'headless/TabGroup/TabGroup';
+import Image from 'headful/Image/Image';
 
 type ProfileCardProps = { profile?: string; title: string; subTitle: string } & React.ComponentProps<
     typeof TabGroupItem
@@ -20,7 +21,7 @@ const ProfileCard = ({ profile, title, subTitle, ...props }: ProfileCardProps) =
     return (
         <TabGroupItem {...props} className={combinedClassName}>
             <FlexBox alignItems="center" gap={12}>
-                <Profile profile={profile} title={title} />
+                <Profile width={32} height={32} profile={profile} title={title} />
                 <FlexBox flexDirection="column" gap={4}>
                     <Text fontSize={14} fontWeight={700} color="#3e4449">
                         {title}
@@ -39,15 +40,17 @@ export default ProfileCard;
 type ProfileProps = {
     profile?: string;
     title: string;
+    width: number;
+    height: number;
 };
 
-const Profile = ({ profile, title }: ProfileProps) => {
+export const Profile = ({ profile, title, width, height }: ProfileProps) => {
     const firstLetterOfTitle = title.charAt(0);
 
     return (
         <>
             {profile ? (
-                <img className={styles.Profile} src={profile} />
+                <Image width={width} height={height} className={styles.Profile} src={profile} />
             ) : (
                 <div className={classNames(styles.Default, styles.Profile)}>{firstLetterOfTitle}</div>
             )}
