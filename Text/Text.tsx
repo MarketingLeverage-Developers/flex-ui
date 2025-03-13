90fdf25b29dc60b2fd3f62ab8983a53ab0d022ca
dhkim <dhkim@mleverage.co.kr>
2025-03-13 18:14:01 +0900

    feat: Text 생성

--- Text/Text.tsx
+++ Text/Text.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import classNames from 'classnames';
+import styles from './Text.module.scss';
 
-type TextProps = React.HTMLAttributes<HTMLDivElement> & {
-    fontSize?: number;
-    fontWeight?: number;
+export type TextProps = React.HTMLAttributes<HTMLDivElement> & {
+    fontSize?: number | string;
+    fontWeight?: number | string;
     color?: string;
     textAlign?: 'left' | 'center' | 'right';
     children: React.ReactNode;
 };
 
-const Text = ({ fontSize, fontWeight, color, textAlign, children, ...props }: TextProps) => {
-    const inlineStyle: React.CSSProperties = {
-        fontSize: `${fontSize ?? 16}px`,
-        fontWeight: fontWeight ?? 400,
-        color: color ?? '#324449',
-        textAlign: textAlign ?? 'left',
+interface CSSPropertiesWithVars extends React.CSSProperties {
+    [key: `--${string}`]: string | number;
+}
+
+const Text = ({ fontSize, fontWeight, color, textAlign, children, style, className, ...props }: TextProps) => {
+    // fontSize가 number이면 'px' 단위를 붙이고, 문자열이면 그대로 사용
+    const computedFontSize = typeof fontSize === 'number' ? `${fontSize}px` : fontSize ?? '16px';
+    // fontWeight도 마찬가지, 필요에 따라 숫자 또는 문자열 허용
+    const computedFontWeight = typeof fontWeight === 'number' ? fontWeight : fontWeight ?? 400;
+    const computedColor = color ?? '#324449';
+    const computedTextAlign = textAlign ?? 'left';
+
+    const cssVariables: CSSPropertiesWithVars = {
+        '--text-font-size': computedFontSize,
+        '--text-font-weight': computedFontWeight,
+        '--text-color': computedColor,
+        '--text-text-align': computedTextAlign,
     };
 
-    const combinedStyle: React.CSSProperties = { ...props.style, ...inlineStyle };
-
     return (
-        <div {...props} style={combinedStyle}>
+        <span {...props} className={classNames(styles.text, className)} style={{ ...cssVariables, ...style }}>
             {children}
-        </div>
+        </span>
     );
 };
 
