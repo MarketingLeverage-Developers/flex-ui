e83ab59d0918133de6e3c6a70ef110deaac1876a
dhkim <dhkim@mleverage.co.kr>
2025-03-14 11:31:18 +0900

    remove: BasicPagination 컴포넌트 삭제 - 구조 변경으로 인한 코드 삭제

--- BasicPagination/PageSizeSelect/PageSizeSelectTrigger.tsx
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/react';
-import BasicSelect from 'headful/BasicSelect/BasicSelect';
-import { FaBook } from 'react-icons/fa';
-
-const PageSizeSelectTrigger = () => {
-    return <BasicSelect.Trigger icon={<FaBook width={14} height={14} />} label={'페이지 사이즈'} />;
-};
-
-export default PageSizeSelectTrigger;
