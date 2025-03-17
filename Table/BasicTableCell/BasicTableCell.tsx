ddeb88f45b1179ee4ec263e66b0d189e23510a72
dhkim <dhkim@mleverage.co.kr>
2025-03-17 13:46:45 +0900

    style: Basictable 및 하위 컴포넌트 수정 - 테이블 스타일 속성 수정

--- Table/BasicTableCell/BasicTableCell.tsx
+++ Table/BasicTableCell/BasicTableCell.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './BasicTableCell.module.scss';
 
 type BasicTableCellProps = {
     children: React.ReactNode;
     align?: 'left' | 'center' | 'right';
+    hasbgColor?: boolean;
+    isEditable?: boolean; // 편집 가능 여부 prop
+    width?: number;
 };
 
-const BasicTableCell = ({ children, align = 'center' }: BasicTableCellProps) => {
-    const alignClass = styles[`basic-table-cell--${align}`];
+const BasicTableCell = ({ children, align = 'center', hasbgColor, isEditable = false, width }: BasicTableCellProps) => {
+    const cellClasses = classNames(
+        styles.BasicTableCell,
+        align === 'center' ? styles.Left : styles.Right,
+        hasbgColor && styles.Background,
+        isEditable && styles.Editable, // 편집 가능 시 추가 스타일 적용 (옵션)
+        width && styles.Fixed
+    );
 
-    return <td className={`${styles['basic-table-cell']} ${alignClass}`}>{children}</td>;
+    const cssVariables: React.CSSProperties = {
+        '--cell-width': `${width}px`,
+    } as React.CSSProperties;
+
+    return (
+        <td className={cellClasses} style={{ ...cssVariables }} contentEditable={isEditable}>
+            {children}
+        </td>
+    );
 };
 
 export default BasicTableCell;
