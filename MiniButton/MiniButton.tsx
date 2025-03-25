21c3342e9a8015c938ce3a0a06d750d2abb11ecb
dhkim <dhkim@mleverage.co.kr>
2025-03-25 14:09:17 +0900

    feat: MiniButton 수정 - waning 스타일 타입 추가

--- MiniButton/MiniButton.tsx
+++ MiniButton/MiniButton.tsx
@@ -3,12 +3,13 @@ import styles from './MiniButton.module.scss';
 import classNames from 'classnames';
 
 type MiniButtonProps = HTMLAttributes<HTMLButtonElement> & {
-    type: 'brand' | 'base';
+    type: 'brand' | 'base' | 'warning';
 };
 
 const MiniButton = ({ type, ...props }: MiniButtonProps) => {
     const combinedClassName = classNames(styles.MiniButton, {
         [styles.Brand]: type === 'brand',
+        [styles.Warning]: type === 'warning',
     });
 
     return <button {...props} className={combinedClassName} />;
