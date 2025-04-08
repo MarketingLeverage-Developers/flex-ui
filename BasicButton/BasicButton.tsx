0162decac1dfe1308b8ce86e5ae0494cfa51bca0
dhkim <dhkim@mleverage.co.kr>
2025-04-08 15:03:40 +0900

    feat: BasicButton 수정 - width 속성 추가

--- BasicButton/BasicButton.tsx
+++ BasicButton/BasicButton.tsx
@@ -4,12 +4,14 @@ import styles from './BasicButton.module.scss';
 type BasicButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     children: React.ReactNode;
     isVisible?: boolean;
+    width?: number | string;
     height?: number;
     variant?: 'solid' | 'gradient';
 };
 
-const BasicButton = ({ children, isVisible = true, height, variant = 'solid', ...props }: BasicButtonProps) => {
+const BasicButton = ({ children, isVisible = true, width, height, variant = 'solid', ...props }: BasicButtonProps) => {
     const inlineStyle: React.CSSProperties = {
+        width: `${width ?? '100%'}`,
         height: `${height ?? 40}px`,
         opacity: isVisible ? 1 : 0,
         background: variant === 'gradient' ? 'linear-gradient(45deg, #f98131, #f6d365)' : '#f98131',
