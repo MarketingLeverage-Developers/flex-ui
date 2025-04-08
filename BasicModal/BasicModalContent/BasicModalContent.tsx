525d72a90a7cc601f26d3bb0e13ea485bc5f42e7
dhkim <dhkim@mleverage.co.kr>
2025-04-08 15:05:29 +0900

    feat: BasicModalContent 수정 - BasicModalContent 기본 스타일 수정

--- BasicModal/BasicModalContent/BasicModalContent.tsx
+++ BasicModal/BasicModalContent/BasicModalContent.tsx
@@ -1,12 +1,20 @@
 import Modal from 'headless/Modal/Modal';
 import styles from './BasicModalContent.module.scss';
+import { HTMLAttributes } from 'react';
+import classNames from 'classnames';
 
-type BasicModalContentProps = {
+type BasicModalContentProps = HTMLAttributes<HTMLDivElement> & {
     children: React.ReactNode;
 };
 
-const BasicModalContent = ({ children }: BasicModalContentProps) => {
-    return <Modal.Content className={styles.BasicModalContent}>{children}</Modal.Content>;
+const BasicModalContent = ({ children, ...props }: BasicModalContentProps) => {
+    const combinedStyle = classNames(styles.BasicModalContent, props.className);
+
+    return (
+        <Modal.Content {...props} className={combinedStyle}>
+            {children}
+        </Modal.Content>
+    );
 };
 
 export default BasicModalContent;
