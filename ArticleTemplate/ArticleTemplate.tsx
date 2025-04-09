2ab260fb22c1e37eda113838eed3e7c54cddd0fb
dhkim <dhkim@mleverage.co.kr>
2025-04-09 20:24:15 +0900

    feat: ArticleTempalte 수정 - category, onTabChange 속성 추가

--- ArticleTemplate/ArticleTemplate.tsx
+++ ArticleTemplate/ArticleTemplate.tsx
@@ -4,37 +4,60 @@ import { ChangeEventHandler } from 'react';
 import BasicInput from 'headful/BasicInput/BasicInput';
 import BasicButton from 'headful/BasicButton/BasicButton';
 import BasicModal from 'headful/BasicModal/BasicModal';
+import { Category } from 'types/model/content';
+import TabGroup from 'headless/TabGroup/TabGroup';
+import RadioTab from 'headful/RadioTab/RadioTab';
+import BasicTab from 'headful/BasicTab/BasicTab';
 
 type ArticleTemplateProps = {
     isModal?: boolean;
     type: 'create' | 'update';
     title: string;
     editor: string;
+    category?: Category;
     onButtonClick: () => void;
     onInputChange: ChangeEventHandler<HTMLInputElement>;
     onEditorChange: React.Dispatch<React.SetStateAction<string>>;
     onImageUpload: (files: FileList) => Promise<any>;
+    onTabChange?: (value: string) => void;
 };
 
 const ArticleTemplate = ({
     type,
     title,
     editor,
+    category,
     onButtonClick,
     onInputChange,
     onEditorChange,
     onImageUpload,
+    onTabChange,
     isModal,
 }: ArticleTemplateProps) => {
     return (
         <div className={styles.ArticleTemplate}>
-            <div className={styles.Head}>
-                <div className={styles.HeaderWrapper}>
-                    <div className={styles.Header}>
+            <div className={styles.HeaderWrapper}>
+                <div className={styles.Header}>
+                    <div className={styles.Left}>
                         <div className={styles.Label}>{type === 'create' ? '작성하기' : '수정하기'} </div>
                         <span className={styles.Title}>{title}</span>
                     </div>
-                    {isModal && <BasicModal.Close />}
+                    <div className={styles.Right}>
+                        {/* {category && onTabChange && (
+                            <BasicTab defaultValue={category}>
+                                <BasicTab.Item value={Category.REVIEW} onTabGroupItemClick={onTabChange}>
+                                    리뷰
+                                </BasicTab.Item>
+                                <BasicTab.Item value={Category.ESTIMATE} onTabGroupItemClick={onTabChange}>
+                                    견적
+                                </BasicTab.Item>
+                                <BasicTab.Item value={Category.CASE} onTabGroupItemClick={onTabChange}>
+                                    사례
+                                </BasicTab.Item>
+                            </BasicTab>
+                        )} */}
+                        {isModal && <BasicModal.Close />}
+                    </div>
                 </div>
                 <div className={styles.InputWrapper}>
                     <BasicInput
