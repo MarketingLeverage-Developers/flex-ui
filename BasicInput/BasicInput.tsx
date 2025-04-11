8fad6689e8553ae0097bba84c785079393f29870
dhkim <dhkim@mleverage.co.kr>
2025-04-11 17:32:35 +0900

    feat: BasicInput 수정 - 커스텀 가능하게 변경

--- BasicInput/BasicInput.tsx
+++ BasicInput/BasicInput.tsx
@@ -5,15 +5,28 @@ import classNames from 'classnames';
 type BasicInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
     left?: React.ReactNode;
     right?: React.ReactNode;
+    fontSize?: string | number;
+    fontWeight?: string | number;
+    variant?: 'border' | 'none';
 };
 
-const BasicInput = ({ left, right, ...props }: BasicInputProps) => {
-    const combinedStyle = classNames(styles['basic-input'], props.className);
+const BasicInput = ({ left, right, fontSize, fontWeight, variant = 'border', ...props }: BasicInputProps) => {
+    type CSSPropertiesWithVars = React.CSSProperties & {
+        [key: `--${string}`]: string | number;
+    };
+    const cssVars: CSSPropertiesWithVars = {
+        '--input-font-size': fontSize ? `${fontSize}px` : '16px',
+        '--input-font-weight': fontWeight ?? 'regular',
+    };
+
+    const combinedStyle = classNames(styles.BasicInput, {
+        [styles.Border]: variant === 'border',
+    });
 
     return (
-        <div className={combinedStyle}>
+        <div className={combinedStyle} style={{ ...cssVars, ...props.style }}>
             {left}
-            <input {...props} className={styles['basic-input__input']} />
+            <input {...props} />
             {right}
         </div>
     );
