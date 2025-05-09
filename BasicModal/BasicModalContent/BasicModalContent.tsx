91da1106970d489e9f172feea8e2f61024e44c56
dhkim <dhkim@mleverage.co.kr>
2025-05-09 14:48:56 +0900

    feat: BasicModal 수정 - 스타일 수정

--- BasicModal/BasicModalContent/BasicModalContent.tsx
+++ BasicModal/BasicModalContent/BasicModalContent.tsx
@@ -1,20 +1,54 @@
-import Modal from 'headless/Modal/Modal';
+import Modal from '@/headless/Modal/Modal';
 import styles from './BasicModalContent.module.scss';
 import { HTMLAttributes } from 'react';
 import classNames from 'classnames';
+import { CSSPropertiesWithVars, HexColor } from '@/ui-kit/types/types';
+import { dimensionToVariable } from '@/ui-kit/libs/utils';
+import Flex from '@/ui-kit/components/layouts/Flex/Flex';
+import Image from '@/ui-kit/components/contents/Image/Image';
+import X from '@/assets/images/x.svg';
+import { config } from '@/ui-kit/configs/config';
 
-type BasicModalContentProps = HTMLAttributes<HTMLDivElement> & {
+type ContentProps = HTMLAttributes<HTMLDivElement> & {
+    title: string;
+    width?: string;
+    height?: string;
+    backgroundColor?: HexColor;
     children: React.ReactNode;
 };
 
-const BasicModalContent = ({ children, ...props }: BasicModalContentProps) => {
-    const combinedStyle = classNames(styles.BasicModalContent, props.className);
+const Content = ({
+    width = '40%',
+    height = 'auto',
+    backgroundColor = config.theme.primaryColor ?? '#E88731',
+    title,
+    children,
+    ...props
+}: ContentProps) => {
+    const combinedStyle = classNames(styles.Content, props.className);
+
+    const contentVariables: CSSPropertiesWithVars = {
+        '--width': dimensionToVariable(width),
+        '--height': dimensionToVariable(height),
+    };
+
+    const headerVariables: CSSPropertiesWithVars = {
+        '--header-color': backgroundColor,
+    };
 
     return (
-        <Modal.Content {...props} className={combinedStyle}>
-            {children}
+        <Modal.Content {...props} className={combinedStyle} style={{ ...contentVariables }}>
+            <div className={styles.Header} style={{ ...headerVariables }}>
+                <Flex justify="space-between">
+                    {title}
+                    <Modal.Close>
+                        <Image image={X} alt="닫기" style={{ cursor: 'pointer' }} width={20} />
+                    </Modal.Close>
+                </Flex>
+            </div>
+            <div className={styles.Body}>{children}</div>
         </Modal.Content>
     );
 };
 
-export default BasicModalContent;
+export default Content;
