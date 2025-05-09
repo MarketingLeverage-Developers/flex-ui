4bc371760fc948769c426334e931e5d67ad8f5a8
dhkim <dhkim@mleverage.co.kr>
2025-05-09 10:29:27 +0900

    feat: MainContent 생성

---
+++ MainContent/MainContent.tsx
@@ -0,0 +1,16 @@
+import Box from '@/ui-kit/components/layouts/Box/Box';
+import React from 'react';
+
+type MainContentProps = {
+    children: React.ReactNode;
+};
+
+const MainContent = ({ children }: MainContentProps) => {
+    return (
+        <Box padding={30} backgroundColor="#f5f5f5" height="100%">
+            {children}
+        </Box>
+    );
+};
+
+export default MainContent;
