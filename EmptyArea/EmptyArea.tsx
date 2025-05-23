db7a05c30c9203187be249361e4c222773322d9f
dhkim <dhkim@mleverage.co.kr>
2025-05-23 16:18:20 +0900

    feat: EmptyArea 수정 - 이미지 및 내부 코드 수정

--- EmptyArea/EmptyArea.tsx
+++ EmptyArea/EmptyArea.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './EmptyArea.module.scss';
-import Flex from 'headful/Flex/Flex';
-import Text from 'headful/Text/Text';
+import Flex from '@/ui-kit/src/components/layouts/Flex/Flex';
+import Text from '@/ui-kit/src/components/contents/Text/Text';
 
 type EmptyAreaProps = {
     icon: React.ReactNode;
@@ -11,21 +11,14 @@ type EmptyAreaProps = {
 
 const EmptyArea = ({ icon, title, subTitle }: EmptyAreaProps) => {
     return (
-        <Flex
-            height={'100%'}
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            gap={16}
-            className={styles.EmptyArea}
-        >
+        <Flex height={'100%'} direction="column" justify="center" align="center" gap={15} className={styles.EmptyArea}>
             {icon}
-            <Text color="var(--base-color-primary)" fontSize="var(--font-size-lg)" fontWeight="var(--font-weight-bold)">
+            <Text color="#474747" size={20} weight={500}>
                 {title}
             </Text>
 
             {subTitle && (
-                <Text color="var(--base-color-tertiary)" fontSize="var(--font-size-sm)">
+                <Text color="#474747" size={16}>
                     {subTitle}
                 </Text>
             )}
