bdf2e3b9645e56441d527dbb3d1889bcd85423fa
dhkim <dhkim@mleverage.co.kr>
2025-06-12 19:05:34 +0900

    style: BasicForm 수정 - 스타일 규격 수정

--- BasicForm/Label/Label.tsx
+++ BasicForm/Label/Label.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './Label.module.scss';
+import classNames from 'classnames';
 
 type LabelProps = {
     text: string;
     required?: boolean;
     children: React.ReactNode;
+    align?: 'left' | 'center' | 'right';
 };
 
-const Label = ({ text, required, children }: LabelProps) => {
+const Label = ({ text, required, align = 'left', children }: LabelProps) => {
+    const cssVariables: React.CSSProperties = {
+        '--align': align,
+    } as React.CSSProperties;
+
     return (
-        <div className={styles.Label}>
+        <div className={styles.Label} style={{ ...cssVariables }}>
             <div className={styles.Text}>
                 <span className={styles.LabelText}>{text}</span>
                 {required && <span className={styles.Required}>*</span>}
