f5a86f27c527cf02a6faaf7ba789534ccf5b0b4f
dhkim <dhkim@mleverage.co.kr>
2025-06-20 14:16:03 +0900

    feat: ColorChip 생성

---
+++ ColorChip/ColorChip.tsx
@@ -0,0 +1,23 @@
+import { CSSPropertiesWithVars, HexColor } from '@/ui-kit/src/types';
+import styles from './ColorChip.module.scss';
+
+type ColorChipProps = {
+    color: HexColor;
+    bgColor: HexColor;
+    text: string;
+};
+
+const ColorChip = ({ color, bgColor, text }: ColorChipProps) => {
+    const cssVariables: CSSPropertiesWithVars = {
+        '--color': color,
+        '--background-color': bgColor,
+    };
+
+    return (
+        <div className={styles.ColorChip} style={{ ...cssVariables }}>
+            {text}
+        </div>
+    );
+};
+
+export default ColorChip;
