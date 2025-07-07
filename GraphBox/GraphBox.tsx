70219c24e91d75c8b385386707f02cdab25e55a1
dhkim <dhkim@mleverage.co.kr>
2025-07-07 19:23:25 +0900

    feat: GraphBox 수정 - Sub 속성 추가

--- GraphBox/GraphBox.tsx
+++ GraphBox/GraphBox.tsx
@@ -1,15 +1,21 @@
 import Box from '@/ui-kit/src/components/layouts/Box/Box';
 import React from 'react';
 import styles from './GraphBox.module.scss';
+import classNames from 'classnames';
 
 type GraphBoxProps = React.ComponentProps<typeof Box> & {
     title?: string;
     rightNode?: React.ReactNode;
+    sub?: boolean;
 };
 
-const GraphBox = ({ children, title, rightNode, ...props }: GraphBoxProps) => {
+const GraphBox = ({ children, title, sub, rightNode, ...props }: GraphBoxProps) => {
+    const className = classNames(styles.GraphBox, {
+        [styles.Sub]: sub,
+    });
+
     return (
-        <div className={styles.GraphBox} {...props}>
+        <div className={className} {...props}>
             <div className={styles.Title}>
                 <span>{title}</span>
                 <div>{rightNode}</div>
