15aaf128eead3b5ed9243a1feb94cb0e3836adf1
dhkim <dhkim@mleverage.co.kr>
2025-07-15 10:48:53 +0900

    feat: ContentBox 수정 - titleBorder 속성 추가

--- ContentBox/ContentBox.tsx
+++ ContentBox/ContentBox.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import styles from './ContentBox.module.scss';
 import { BoxSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
 import { dimensionToString, dimensionToVariable, spacingToSpace, spacingToString } from '@/ui-kit/src/utils';
+import classNames from 'classnames';
 
 type ContentBoxProps = React.ComponentProps<typeof Box> & {
     title?: React.ReactNode;
+    titleBorder?: boolean;
     header?: React.ReactNode;
     rightNode?: React.ReactNode;
     width?: BoxSize | string;
@@ -13,17 +15,31 @@ type ContentBoxProps = React.ComponentProps<typeof Box> & {
     s?: boolean;
 };
 
-const ContentBox = ({ children, title, header, rightNode, s, width, height, ...props }: ContentBoxProps) => {
+const ContentBox = ({
+    children,
+    title,
+    titleBorder,
+    header,
+    rightNode,
+    s,
+    width,
+    height,
+    ...props
+}: ContentBoxProps) => {
     const cssVariables: CSSPropertiesWithVars = {
         '--width': s ? dimensionToString(width) : dimensionToVariable(width),
         '--height': s ? dimensionToString(height) : dimensionToVariable(height),
         '--padding': s ? spacingToString(props.padding) : spacingToSpace(props.padding),
     };
 
+    const TitleClassName = classNames(styles.Title, {
+        [styles.Border]: titleBorder,
+    });
+
     return (
         <div className={styles.ContentBox} {...props} style={{ ...props.style, ...cssVariables }}>
             {(title || header || rightNode) && (
-                <div className={styles.Title}>
+                <div className={TitleClassName}>
                     {title && <span>{title}</span>}
                     {header && <div>{header}</div>}
                     <div>{rightNode}</div>
