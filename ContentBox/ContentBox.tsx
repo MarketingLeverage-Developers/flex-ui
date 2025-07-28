9774c52269532bec99a52029fdffc6f1bcf41ca1
dhkim <dhkim@mleverage.co.kr>
2025-07-28 13:59:13 +0900

    feat: ContentBox 수정 - titleAlign 속성 추가

--- ContentBox/ContentBox.tsx
+++ ContentBox/ContentBox.tsx
@@ -2,26 +2,30 @@ import Box from '@/ui-kit/src/components/layouts/Box/Box';
 import React from 'react';
 import styles from './ContentBox.module.scss';
 import { BoxSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
-import { dimensionToString, dimensionToVariable, spacingToSpace, spacingToString } from '@/ui-kit/src/utils';
+import { dimensionToString, dimensionToVariable, spacingToSpace, spacingToString, toFont } from '@/ui-kit/src/utils';
 import classNames from 'classnames';
 
 type ContentBoxProps = React.ComponentProps<typeof Box> & {
     title?: React.ReactNode;
+    titleAlign?: 'space-between' | 'center' | 'end' | 'start';
     titleBorder?: boolean;
     header?: React.ReactNode;
     rightNode?: React.ReactNode;
     width?: BoxSize | string;
     height?: BoxSize | string;
+    fontSize?: number | string;
     s?: boolean;
 };
 
 const ContentBox = ({
     children,
     title,
+    titleAlign = 'space-between',
     titleBorder,
     header,
     rightNode,
     s,
+    fontSize = 17,
     width,
     height = '100%',
     ...props
@@ -30,6 +34,8 @@ const ContentBox = ({
         '--width': s ? dimensionToString(width) : dimensionToVariable(width),
         '--height': s ? dimensionToString(height) : dimensionToVariable(height),
         '--padding': s ? spacingToString(props.padding) : spacingToSpace(props.padding),
+        '--font-size': s ? dimensionToString(fontSize) : toFont(fontSize),
+        '--title-align': titleAlign,
     };
 
     const TitleClassName = classNames(styles.Title, {
