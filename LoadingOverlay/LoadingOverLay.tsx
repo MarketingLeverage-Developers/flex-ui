f648b70b467dd5248d4339f6cdda36b5c4a33e47
dhkim <dhkim@mleverage.co.kr>
2025-07-29 18:09:00 +0900

    feat: LoadingoverLay 수정 - DeferredComponent 적용

--- LoadingOverlay/LoadingOverLay.tsx
+++ LoadingOverlay/LoadingOverLay.tsx
@@ -3,13 +3,15 @@ import styles from './LoadingOverLay.module.scss';
 import animationData from 'assets/etc/loading.json';
 import Lottie from 'react-lottie';
 import classNames from 'classnames';
+import DeferredComponent from '@/components/DeferredComponent/DeferredComponent';
 
 type LoadingOverlayProps = {
     children?: React.ReactNode;
-    isInitial?: boolean;
+    isLoading?: boolean; // ✅ 이름 변경됨
+    isFetching?: boolean;
 };
 
-const LoadingOverlay = ({ children, isInitial = false }: LoadingOverlayProps) => {
+const LoadingOverlay = ({ children, isLoading = false, isFetching = false }: LoadingOverlayProps) => {
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -19,10 +21,10 @@ const LoadingOverlay = ({ children, isInitial = false }: LoadingOverlayProps) =>
         },
     };
 
-    return (
+    const content = (
         <div
             className={classNames(styles.LoadingOverlay, {
-                [styles.Initial]: isInitial,
+                [styles.Initial]: isLoading, // ✅ 여기서도 이름 변경
             })}
         >
             <div style={{ width: 40, height: 40 }}>
@@ -31,6 +33,10 @@ const LoadingOverlay = ({ children, isInitial = false }: LoadingOverlayProps) =>
             {children}
         </div>
     );
+
+    if (isLoading) return content;
+
+    return <DeferredComponent active={isFetching}>{content}</DeferredComponent>;
 };
 
 export default LoadingOverlay;
