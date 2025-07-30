e06d85c79ec2ab9fb38910463c1af64564d292dd
mlyykkang <yykkang@mleverage.co.kr>
2025-07-30 15:16:00 +0900

    fix: ImageCard 수정 - ImageModal 추가

--- ImageCard/ImageCard.tsx
+++ ImageCard/ImageCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './ImageCard.module.scss';
 import { HiXMark } from 'react-icons/hi2';
 import { LiaDownloadSolid } from 'react-icons/lia';
+import ImageModal from '../ImageModal/ImageModal';
+import Image from '@/ui-kit/src/components/contents/Image/Image';
 
 type ImageCardProps = {
     text: string;
@@ -12,36 +14,53 @@ type ImageCardProps = {
 
 const ImageCard = ({ text, image, onDeleteButtonClick, onDownloadButtonClick }: ImageCardProps) => {
     return (
-        <div className={styles.ImageCard}>
-            <div className={styles.ImageBox}>
-                <img src={image} alt="이미지" />
-                {onDeleteButtonClick && (
-                    <button
-                        className={styles.Delete}
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            onDeleteButtonClick();
-                        }}
-                    >
-                        <HiXMark className={styles.Icon} />
-                    </button>
-                )}
-                {onDownloadButtonClick && (
-                    <button
-                        className={styles.Download}
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            onDownloadButtonClick();
-                        }}
-                    >
-                        <LiaDownloadSolid className={styles.Icon} />
-                    </button>
-                )}
+        <ImageModal>
+            <div className={styles.ImageCard}>
+                <div className={styles.ImageBox}>
+                    <img src={image} alt="이미지" />
+                    {onDeleteButtonClick && (
+                        <button
+                            className={styles.Delete}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                onDeleteButtonClick();
+                            }}
+                        >
+                            <HiXMark className={styles.Icon} />
+                        </button>
+                    )}
+                    {onDownloadButtonClick && (
+                        <button
+                            className={styles.Download}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                onDownloadButtonClick();
+                            }}
+                        >
+                            <LiaDownloadSolid className={styles.Icon} />
+                        </button>
+                    )}
+                </div>
+                <div className={styles.Text}>
+                    <span>{text}</span>
+                </div>
             </div>
-            <div className={styles.Text}>
-                <span>{text}</span>
-            </div>
-        </div>
+            {image.includes('mp4') ? (
+                <video
+                    src={image}
+                    controls
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    width="100%"
+                    height="100%"
+                    style={{ objectFit: 'cover' }}
+                />
+            ) : (
+                <Image image={image} width="100%" height="100%" />
+            )}
+        </ImageModal>
     );
 };
 
